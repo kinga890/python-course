d275c617764aa7537c0ba39f101a95c47110be18
kinga890 <fryckinga1@gmail.com>
2025-03-03 21:57:13 +0100

    wynik jako list  with '#' will be ignored, and an empty message aborts the commit.

--- counter.py
+++ counter.py
@@ -1,6 +1,6 @@
 my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-print('1 sposób')
+print('sposób A')
 prev = None
 
 for value in my_list:
@@ -16,7 +16,7 @@ print(result)  # żeby wyświetlić tylko końcową sumę
 
 my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-print('2 sposób')
+print('sposób B')
 prev = my_list[0]
 print(my_list[0])
 for value in my_list[1:]:
@@ -24,21 +24,34 @@ for value in my_list[1:]:
     prev = result
     print(result)
 
+print('sposób C')
+
+print('KACPER PRZECZYTAJ TO ROZIWĄZANIE')
+
 my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-my_set=set()
+counter = 0
+for value in my_list:
+    counter=counter + value
+    print(counter)
+
+
+# wynik jako lista
+
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+result_list=list()
+
 prev = None
 
 for value in my_list:
     if prev is None:
         result = value
         prev = value
-        my_set.add(value)
+        result_list.append(value)  #dodaję element do końcowej listy
 
     else:
         result = prev + value
         prev = result
-        my_set.add(result)
-list = list(my_set)
-list.sort()
-print(list)
+        result_list.append(result)   #dodaję element do końcowej listy
+print(type(result_list))
+print((result_list))
 
