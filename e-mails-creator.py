0becd035c4beb781290b83d74bdc77652e74dc7d
Kacper <54122071+kacper-mush@users.noreply.github.com>
2025-03-04 22:03:39 +0100

    quick review with suggested improvements
    Suggested fixes for some redundancy, simpler code and better naming conventions.

--- e-mails-creator.py
+++ e-mails-creator.py
@@ -1,18 +1,18 @@
 
 
-student_id= input('Enter student\'s id or type \'exit\' ')
+student_id= input('Enter student\'s id or type \'exit\' ')      # FIXME: this part is redundant
 domain= '@example.com'
 
-email=student_id + domain
+email=student_id + domain                                       # FIXME: and this
 email_list=[]
 
 while True:
     student_id= input('Enter student\'s id or type \'exit\' ')
     email = student_id + domain
-    email_list.append(email)
-    if student_id == 'exit':
+    email_list.append(email)                                  
+    if student_id == 'exit':                                   # FIXME: this check can be moved so that the pop will not be needed
         break
 email_list.pop() #usuwam ostatni e-mail "exit@example.pl'
 
-for _ in email_list:
-    print(_)
+for _ in email_list:                                           # NEAT: "_" is reserved for variables that are unused, this one is
+    print(_)                                                   # so it should be named appropriately (e.g. "email")
