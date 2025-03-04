e8092b558bb88a9f73b2bcde0f034e0f6d6bf3e7
kinga890 <fryckinga1@gmail.com>
2025-03-04 22:13:33 +0100

    test3

--- e-mails-creator.py
+++ e-mails-creator.py
@@ -6,13 +6,24 @@ domain= '@example.com'
 email=student_id + domain
 email_list=[]
 
-while True:
-    student_id= input('Enter student\'s id or type \'exit\' ')
-    email = student_id + domain
-    email_list.append(email)
-    if student_id == 'exit':
-        break
+if student_id:
+    while True:
+        student_id= input('Enter student\'s id or type \'exit\' ')
+        email = student_id + domain
+        email_list.append(email)
+        if student_id == 'exit':
+            break
+else:
+    print('enter vaild id')
 email_list.pop() #usuwam ostatni e-mail "exit@example.pl'
 
+email_list=set(email_list)  #aby e-emaile się nie powtarzały
 for _ in email_list:
     print(_)
+
+email = input('username')
+
+if email:
+    print('not value')
+else:
+    print('not value')
