a2e21c6a0bb8c581f01510c63e3c6949884145f4
kinga890 <fryckinga1@gmail.com>
2025-03-05 01:34:17 +0100

    variable fix  Please enter the commit message for your changes. Lines starting

--- e-mails-creator.py
+++ e-mails-creator.py
@@ -1,22 +1,23 @@
 #(a student's id is a six-digit number)
 
-domain= '@example.com'
+DOMAIN= '@example.com'
 email_list=[]
 
 while True:
     student_id= input('Enter student\'s id or type \'exit\' ')
-    email = student_id + domain
+    email = student_id + DOMAIN
 
     if student_id == 'exit':
         break
     if email in email_list:
         print('email already exist')
+        continue
     if len(student_id) == 6 and student_id.isnumeric() == True:
         email_list.append(email)
     else:
         print('invalid id number')
 
 email_list.sort()
-email_list=set(email_list)
+
 for email_easy_access in email_list:
     print(email_easy_access)
